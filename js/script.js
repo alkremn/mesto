01e07ddffea3bc3422cd3ad05ae32995f609dab7
Alexey Kremnev <alkremn at gmail dot com>
2021-03-09 22:20:47 -0700

    feat: add delete button for card

--- js/script.js
+++ js/script.js
@@ -1,13 +1,72 @@
-let editButton = document.querySelector('.profile__edit-button');
-let closeFormButton = document.querySelector('.popup__close-button');
-let formElement = document.querySelector('.popup__container');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
 
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
+window.onload = () => {
+  const elementsList = document.querySelector('.elements__list');
+  const elementTemplate = document.querySelector('#element__template').content;
 
-let popup = document.querySelector('.popup');
-let nameInput = document.querySelector('.popup__input_type_name');
-let jobInput = document.querySelector('.popup__input_type_job');
+  // <img class="element__image" src="images/venecia.jpg" alt="Венеция">
+  // <div class="element__description">
+  //   <h3 class="element__title">Венеция</h3>
+  //   <button class="element__like-button" type="button"></button>
+  // </div>
+  const cards = initialCards.map(item => {
+    const element = elementTemplate.querySelector('.element').cloneNode(true);
+    const elementImage = element.querySelector('.element__image');
+    const elementTitle = element.querySelector('.element__title');
+    const elementButton = element.querySelector('.element__like-button');
+    elementImage.src = item.link;
+    elementImage.alt = item.name;
+    elementTitle.textContent = item.name;
+    elementButton.addEventListener('click', () => {
+      elementButton.classList.toggle('element__like-button_active');
+    });
+    return element;
+  });
+  console.log(cards);
+  elementsList.append(...cards);
+};
+
+const editButton = document.querySelector('.profile__edit-button');
+const closeFormButton = document.querySelector('.popup__close-button');
+const formElement = document.querySelector('.popup__container');
+
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+
+const popup = document.querySelector('.popup');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_job');
 
 editButton.addEventListener('click', function () {
   document.body.style.overflow = 'hidden';
@@ -30,8 +89,9 @@ closeFormButton.addEventListener('click', function () {
 
 formElement.addEventListener('submit', function (e) {
   e.preventDefault();
-  let name = nameInput.value;
-  let job = jobInput.value;
+  console.log(initialCards);
+  const name = nameInput.value;
+  const job = jobInput.value;
   profileName.textContent = name;
   profileJob.textContent = job;
   popup.classList.remove('popup_opened');
