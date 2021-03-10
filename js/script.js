c43da6df31eb43fdd3f0fddc0a22708f864358dd
Alexey Kremnev <alkremn at gmail dot com>
2021-03-10 12:32:26 -0700

    feat: add card form popup

--- js/script.js
+++ js/script.js
@@ -31,69 +31,144 @@ const initialCards = [
   },
 ];
 
-window.onload = () => {
-  const elementsList = document.querySelector('.elements__list');
-  const elementTemplate = document.querySelector('#element__template').content;
+const EDIT_TITLE = 'Редактировать профиль';
+const ADD_TITLE = 'Новое место';
 
-  // <img class="element__image" src="images/venecia.jpg" alt="Венеция">
-  // <div class="element__description">
-  //   <h3 class="element__title">Венеция</h3>
-  //   <button class="element__like-button" type="button"></button>
-  // </div>
-  const cards = initialCards.map(item => {
-    const element = elementTemplate.querySelector('.element').cloneNode(true);
-    const elementImage = element.querySelector('.element__image');
-    const elementTitle = element.querySelector('.element__title');
-    const elementButton = element.querySelector('.element__like-button');
-    elementImage.src = item.link;
-    elementImage.alt = item.name;
-    elementTitle.textContent = item.name;
-    elementButton.addEventListener('click', () => {
-      elementButton.classList.toggle('element__like-button_active');
-    });
-    return element;
+window.onload = () => {
+  const cards = initialCards.map(card => {
+    return createCardElement(card);
   });
-  console.log(cards);
+
   elementsList.append(...cards);
 };
 
+const elementsList = document.querySelector('.elements__list');
 const editButton = document.querySelector('.profile__edit-button');
-const closeFormButton = document.querySelector('.popup__close-button');
-const formElement = document.querySelector('.popup__container');
-
+const addButton = document.querySelector('.profile__add-button');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
 const popup = document.querySelector('.popup');
-const nameInput = document.querySelector('.popup__input_type_name');
-const jobInput = document.querySelector('.popup__input_type_job');
 
-editButton.addEventListener('click', function () {
+editButton.addEventListener('click', () => {
+  const formTemplate = document.querySelector('#form-template').content;
+  const editForm = formTemplate
+    .querySelector('.popup__container')
+    .cloneNode(true);
+
+  editForm.name = 'profile-form';
+  editForm
+    .querySelector('.popup__close-button')
+    .addEventListener('click', handlePopupClose);
+  editForm
+    .querySelector('.popup__save-button')
+    .addEventListener('click', e => handlePopupSave(e, editForm.name));
+
+  const popupTitle = editForm.querySelector('.popup__title');
+  popupTitle.textContent = EDIT_TITLE;
+  const inputs = editForm.querySelectorAll('.popup__input');
+
+  inputs[0].name = 'profileName';
+  inputs[0].value = profileName.textContent;
+
+  inputs[1].name = 'profileJob';
+  inputs[1].value = profileJob.textContent;
+
+  popup.appendChild(editForm);
   document.body.style.overflow = 'hidden';
   popup.classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
 });
 
-popup.addEventListener('click', function (e) {
+addButton.addEventListener('click', () => {
+  const formTemplate = document.querySelector('#form-template').content;
+  const addForm = formTemplate
+    .querySelector('.popup__container')
+    .cloneNode(true);
+
+  addForm.name = 'add-form';
+  addForm
+    .querySelector('.popup__close-button')
+    .addEventListener('click', handlePopupClose);
+
+  const saveButton = addForm.querySelector('.popup__save-button');
+  saveButton.textContent = 'Создать';
+  saveButton.addEventListener('click', e => handlePopupSave(e, addForm.name));
+
+  const popupTitle = addForm.querySelector('.popup__title');
+  popupTitle.textContent = ADD_TITLE;
+
+  const inputs = addForm.querySelectorAll('.popup__input');
+  inputs[0].placeholder = 'Название';
+  inputs[0].name = 'placeName';
+  inputs[1].placeholder = 'Ссылка на картинку';
+  inputs[1].name = 'placeLink';
+
+  popup.appendChild(addForm);
+  document.body.style.overflow = 'hidden';
+  popup.classList.add('popup_opened');
+});
+
+popup.addEventListener('click', e => {
   if (e.target.className === 'popup popup_opened') {
-    popup.classList.remove('popup_opened');
-    document.body.style.overflow = 'auto';
+    handlePopupClose();
   }
 });
 
-closeFormButton.addEventListener('click', function () {
-  popup.classList.remove('popup_opened');
-  document.body.style.overflow = 'auto';
-});
-
-formElement.addEventListener('submit', function (e) {
+const handlePopupSave = (e, formName) => {
   e.preventDefault();
-  console.log(initialCards);
+  if (formName === 'profile-form') {
+    handleProfileSave();
+  } else {
+    handleCardSave();
+  }
+  handlePopupClose();
+};
+
+const handlePopupClose = () => {
+  popup.classList.remove('popup_opened');
+  const child = popup.children[0];
+  popup.removeChild(child);
+  document.body.style.overflow = 'auto';
+};
+
+const handleProfileSave = () => {
+  const nameInput = document.querySelector('input[name="profileName"]');
+  const jobInput = document.querySelector('input[name="profileJob"]');
   const name = nameInput.value;
   const job = jobInput.value;
   profileName.textContent = name;
   profileJob.textContent = job;
-  popup.classList.remove('popup_opened');
-  document.body.style.overflow = 'auto';
-});
+};
+
+const handleCardSave = () => {
+  const nameInput = document.querySelector('input[name="placeName"]');
+  const linkInput = document.querySelector('input[name="placeLink"]');
+  const name = nameInput.value;
+  const link = linkInput.value;
+  const card = createCardElement({ name, link });
+  elementsList.prepend(card);
+};
+
+// helper methods
+
+const createCardElement = item => {
+  const elementTemplate = document.querySelector('#element__template').content;
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  const elementImage = element.querySelector('.element__image');
+  const elementTitle = element.querySelector('.element__title');
+  const deleteButton = element.querySelector('.element__delete-button');
+  const likeButton = element.querySelector('.element__like-button');
+
+  elementImage.src = item.link;
+  elementImage.alt = item.name;
+  elementTitle.textContent = item.name;
+
+  deleteButton.addEventListener('click', () => {
+    elementsList.removeChild(element);
+  });
+
+  likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle('element__like-button_active');
+  });
+  return element;
+};
